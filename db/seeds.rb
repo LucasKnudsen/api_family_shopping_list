3bdf2d6096e09e0dd47a63c118152bbced1cab90
Lucas Knudsen <laksteelhouse@gmail.com>
2021-04-11 12:31:20 +0200

    Changes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-items = ['Tandpasta', 'Slik', 'Öl', 'Chips', 'Måske lidt mad']
+items = ['Tandpasta', 'Slik', 'Öl', 'Chips', 'Måske lidt mad', 'En rigtig lang sötning som skal gå ned på to linjer', 'Mere öl', 'Morgenmad', 'Fisk', 'Mer fisk']
 
 items.each do |item|
   Grocery.create(name: item)
